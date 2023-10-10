c1084ec3da5fb1f9fddb1c4cd3ad7659b4fbb999
bmagario <brian.magario@gmail.com>
2023-10-10 15:35:59 -0300

    kafka configs

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/bmagario/saga/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.bmagario.saga.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
